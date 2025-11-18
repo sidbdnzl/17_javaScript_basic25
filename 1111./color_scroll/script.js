a4d9a83f05c702ee6330287149fa85ffcb13d0b9
sidbdnzl <yk.f070114@gmail.com>
2025-11-18 16:52:34 +0900

    1118

--- 1111./color_scroll/script.js
+++ 1111./color_scroll/script.js
@@ -30,6 +30,24 @@ document.body.style.backgroundColor =
     settingColors[0].b +
     ')';
 
+// 10.関数の定義
+function changeColor(index) {
+    const color = settingColors[index];
+    document.body.style.backgroundColor =
+        `rgb(${color.r}, ${color.g}, ${color.b})`;
+}
+
+function colorChange() {
+    document.body.style.backgroundColor =
+        'rgb(' +
+        settingColors[0].r +
+        ',' +
+        settingColors[0].g +
+        ',' +
+        settingColors[0].b +
+        ')';
+}
+
 // 4.スクロールするたびにイベント
 window.addEventListener('scroll', function () {
     console.log('スクローーーーる');
@@ -38,47 +56,35 @@ window.addEventListener('scroll', function () {
     console.log(scrollY);
     // 6.1 / 4進んだら色が変わるようにする
     // 7.スクロールを4分割
-    if (scrollY < (scrollable * 1) / 4) {
-        document.body.style.backgroundColor =
-            'rgb(' +
-            settingColors[0].r +
-            ',' +
-            settingColors[0].g +
-            ',' +
-            settingColors[0].b +
-            ')';
-    } else if (scrollY < (scrollable * 1) / 2) {
-        document.body.style.backgroundColor =
-            'rgb(' +
-            settingColors[1].r +
-            ',' +
-            settingColors[1].g +
-            ',' +
-            settingColors[1].b +
-            ')';
-    } else if (scrollY < (scrollable * 3) / 4) {
-        document.body.style.backgroundColor =
-            'rgb(' +
-            settingColors[2].r +
-            ',' +
-            settingColors[2].g +
-            ',' +
-            settingColors[2].b +
-            ')';
+    // 9.変数scrollableを4分割
+    // 11.関数の呼び出し（実行）の実引数
+    if (scrollY <= scrollable / 4) {
+        changeColor(0);
+    } else if (scrollY <= scrollable / 2) {
+        changeColor(1);
+    } else if (scrollY <= (scrollable * 3) / 4) {
+        changeColor(2);
     } else {
-        document.body.style.backgroundColor =
-            'rgb(' +
-            settingColors[3].r +
-            ',' +
-            settingColors[3].g +
-            ',' +
-            settingColors[3].b +
-            ')';
+        changeColor(3);
     }
 });
 
+const dummyContent = document.querySelector('.dummy');
+for (let i = 0; i < 1000; i++) {
+    dummyContent.textContent += `テキスト${i}`;
+}
 
+window.addEventListener('scroll', function () {
+    const button = document.querySelector('.moveToTop');
+    if (this.window.scrollY >= 200) {
+        button.style.display = 'block';
+    }
+});
 
-// 9.変数scrollableを4分割
-// 10.関数の定義
-// 11.関数の呼び出し（実行）の実引数
+const button = document.querySelector('.moveToTop');
+button.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
---
+++ 1118_check/script.js
@@ -0,0 +1,14 @@
+
+const btn = document.querySelector('button');
+console.log(btn)
+
+const Menu = document.querySelector('.menu');
+console.log(Menu)
+
+
+const gnav = document.getElementById('.gnav');
+console.log(gnav);
+
+btn.addEventListener('click', function () {
+
+});
